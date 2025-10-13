99860c557d24a90c0ae7cb62ea87b724d4a23ac5
gv646 <surolia.gunjan.96@gmail.com>
2025-10-13 22:26:42 +1000

    adding the dependencies related to embabel. Initialised the spring boot application

---
+++ src/main/java/com/texttosql/text_to_sql_agent/TextToSqlAgentApplication.java
@@ -0,0 +1,13 @@
+package com.texttosql.text_to_sql_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextToSqlAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextToSqlAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/texttosql/text_to_sql_agent/TextToSqlAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.texttosql.text_to_sql_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextToSqlAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
