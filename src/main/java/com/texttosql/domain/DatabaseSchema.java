7036dd8674a6adac768907c7cdfa3bc6be0c2bf0
gv646 <surolia.gunjan.96@gmail.com>
2025-10-20 16:28:35 +1000

    corrected the Builder import

--- src/main/java/com/texttosql/domain/DatabaseSchema.java
+++ src/main/java/com/texttosql/domain/DatabaseSchema.java
@@ -3,17 +3,15 @@ package com.texttosql.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-import groovy.transform.builder.Builder;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 
 /**
  * Represents the complete database schema metadata
  * Contains all tables in the database
  */
-
 @Data
 @Builder
 @NoArgsConstructor
